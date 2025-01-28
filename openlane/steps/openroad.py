bf6d839f7a94ba5afc0eb980d43f30ae7ddace98
mole99 <leomoser99@gmail.com>
2025-01-28 15:17:27 +0100

    Apply black formatting
    Signed-off-by: Leo Moser <leo.moser@efabless.com>

--- openlane/steps/openroad.py
+++ openlane/steps/openroad.py
@@ -2199,9 +2199,7 @@ class WriteAbstractLEF(OpenROADStep):
 
     id = "OpenROAD.WriteAbstractLEF"
     name = "Write Abstract LEF"
-    outputs = [
-        DesignFormat.LEF
-    ]
+    outputs = [DesignFormat.LEF]
 
     def get_script_path(self):
         return os.path.join(get_script_dir(), "openroad", "write_abstract_lef.tcl")
@@ -2215,9 +2213,7 @@ class WriteCDL(OpenROADStep):
 
     id = "OpenROAD.WriteCDL"
     name = "Write CDL"
-    outputs = [
-        DesignFormat.CDL
-    ]
+    outputs = [DesignFormat.CDL]
 
     def get_script_path(self):
         return os.path.join(get_script_dir(), "openroad", "write_cdl.tcl")
